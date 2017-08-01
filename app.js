6bdae27c068b685224fd7f55a2c6609f08a49eeb
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-08-01 18:20:11 +0530

    setting up of controllers in todo application development
    setting up app.js and controllers in scheduler app

---
+++ app.js
@@ -0,0 +1,15 @@
+var express        = require('express');
+var todoController = require('./controllers/todoController');
+
+var app            = express();
+
+app.set('view engine', 'ejs');
+
+//app.use('/public', express.static('./public'));
+app.use(express.static('./public')); //study deeply
+
+todoController(app);
+
+app.listen(3000);
+
+console.log('we are listening to the port 3000');
