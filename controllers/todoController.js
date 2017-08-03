a142191fbefe1a25f7141290810379d0d7a62dd7
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-08-03 23:17:08 +0530

    changes
    changes

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -3,40 +3,79 @@ var bodyParser       = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://test:test@ds127963.mlab.com:27963/todo', { useMongoClient: true })
+mongoose.connect('mongodb://gamwebdev:gamwebdev@ds041841.mlab.com:41841/gamwebdevster123', { useMongoClient: true })
 
 
 var todoSchema = new mongoose.Schema(function(){
-  stock: String
+  item: String
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
 
-var itemOne = Todo({stock: "water flowers"}).save(function(err){
+/*
+var itemOne = Todo({item: "water flowers"}).save(function(err){
   if (err) throw err;
   console.log('item saved');
 });
+*/
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'movies'}];
+/*
+localhost
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/testaroo', { useMongoClient: true });
+
+mongoose.connection.once('open', function(){
+  console.log('connection has been established');
+}).on('error', function(error){
+  console.log('connection has been terminated');
+});
+*/
+
+/*
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://gamwebdev:gamwebdev@ds041841.mlab.com:41841/gamwebdevster123', { useMongoClient: true })
+
+
+var todoSchema = new mongoose.Schema(function(){
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+var itemOne = Todo({item: "water flowers"}).save(function(err){
+  if (err) throw err;
+  console.log('item saved');
+});
+*/
+
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app){
 
   app.get('/todo', function(req, res){
-    res.render('todo', { todos:data });
+    Todo.find({}, function(err, data){
+      if (err) throw err;
+      res.render('todo', { todos:data });
+    });
+
   });
 
   app.post('/todo', urlencodedParser, function(req, res){
-    data.push(req.body);
-    res.json(data); // response to the ajax todolist.js should be json form
+    var newTodo = Todo(req.body).save(function(err, data){
+      if (err) throw err;
+      res.json(data);
+    });
   });
 
   app.delete('/todo/:item', function(req, res){
-    data = data.filter(function(todo){
-      return todo.item.replace(/ /g, '-') !== req.params.item;
-  });
-
-    res.json(data);
+    Todo.find({item: req.params.item.relace(/\-/g, " ")}).remove(function(err, data){
+      if (err) throw err;
+      res.json(data);
+    });
   });
 
 };
